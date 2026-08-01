5711932cf537b43bbe2777b4e49f3fbc6d336f8f
roenstore <thomasmugiprasetyo@gmail.com>
2026-08-01 23:02:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -307,8 +307,8 @@ const testimonialsList = [
 ];
 
 const socialLinks = [
-    { name: "WhatsApp", icon: "fa-brands fa-whatsapp", url: "https://whatsapp.com" },
-    { name: "Instagram", icon: "fa-brands fa-instagram", url: "https://instagram.com" },
+    { name: "WhatsApp", icon: "fa-brands fa-whatsapp", url: "https://wa.link/s3dmn1" },
+    { name: "Instagram", icon: "fa-brands fa-instagram", url: "https://www.instagram.com/banh_roenz" },
     { name: "Facebook", icon: "fa-brands fa-facebook", url: "https://facebook.com" },
     { name: "GitHub", icon: "fa-brands fa-github", url: "https://github.com" },
     { name: "Telegram", icon: "fa-brands fa-telegram", url: "https://telegram.org" },
